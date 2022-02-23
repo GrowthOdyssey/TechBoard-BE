acbe204e76aa6122ea240a975185ceac45c79273
ryo-ima <i.riino5y@gmail.com>
2022-02-23 14:21:56 +0900

    fix: ハンドラーを軽く、コントローラを重くしてみた

--- app/controllers/threadsController.go
+++ app/controllers/threadsController.go
@@ -14,33 +14,9 @@ func threadsHandler(w http.ResponseWriter, r *http.Request) {
 	allowCors(w)
 	switch r.Method {
 	case http.MethodGet:
-		categoryId := r.FormValue("categoryId")
-		page := r.FormValue("page")
-		perPage := r.FormValue("perPage")
-		threads := getThreads(categoryId, page, perPage)
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		json.NewEncoder(w).Encode(threads)
+		getThreads(w, r)
 	case http.MethodPost:
-		accessToken := r.Header.Get("accessToken")
-		if accessToken == "" {
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		}
-
-		var reqBody struct {
-			ThreadTitle string `json:"threadTitle"`
-			CategoryId  string `json:"categoryId"`
-		}
-		err := json.NewDecoder(r.Body).Decode(&reqBody)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		newThreadId := postThread(accessToken, reqBody.ThreadTitle, reqBody.CategoryId)
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		json.NewEncoder(w).Encode(newThreadId)
-
+		postThread(w, r)
 	case http.MethodOptions:
 		w.WriteHeader(http.StatusOK)
 	default:
@@ -54,25 +30,10 @@ func threadsIdHandler(w http.ResponseWriter, r *http.Request) {
 	id := strings.TrimSuffix(strings.TrimPrefix(r.URL.Path, "/v1/threads/"), "/comments")
 	switch r.Method {
 	case http.MethodGet:
-		thread := getThreadById(id)
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		json.NewEncoder(w).Encode(thread)
+		getThreadById(w, r, id)
 	// MEMO URLにcomments入っているか判定してハンドリングしたいかも
 	case http.MethodPost:
-		var reqBody struct {
-			UserId       string `json:"userId"`
-			SessionId    string `json:"sessionId"`
-			CommentTitle string `json:"commentTitle"`
-		}
-		err := json.NewDecoder(r.Body).Decode(&reqBody)
-		if err != nil {
-			fmt.Println(err)
-		}
-		comment := postThreadComments(id, reqBody.UserId, reqBody.SessionId, reqBody.CommentTitle)
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		json.NewEncoder(w).Encode(comment)
+		postThreadComments(w, r, id)
 	case http.MethodOptions:
 		w.WriteHeader(http.StatusOK)
 	default:
@@ -87,10 +48,7 @@ func threadsCategoriesHandler(w http.ResponseWriter, r *http.Request) {
 	allowCors(w)
 	switch r.Method {
 	case http.MethodGet:
-		categories := getThreadsCategories()
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		json.NewEncoder(w).Encode(categories)
+		getThreadsCategories(w, r)
 	case http.MethodOptions:
 		w.WriteHeader(http.StatusOK)
 	default:
@@ -103,31 +61,72 @@ func threadsCategoriesHandler(w http.ResponseWriter, r *http.Request) {
 // コントローラ関数
 
 // スレッド一覧取得
-func getThreads(categoryId, page, perPage string) *models.ThreadsAndPagination {
+func getThreads(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("スレッド一覧取得処理")
-	return models.GetThreadsSql(categoryId, page, perPage)
+	categoryId := r.FormValue("categoryId")
+	page := r.FormValue("page")
+	perPage := r.FormValue("perPage")
+	threads := models.GetThreadsSql(categoryId, page, perPage)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(threads)
 }
 
 // スレッド作成
-func postThread(accessToken, threadTitle, categoryId string) *models.NewThreadId {
+func postThread(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("スレッド作成処理")
-	return models.PostThreadSql(accessToken, threadTitle, categoryId)
+	accessToken := r.Header.Get("accessToken")
+	if accessToken == "" {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+
+	var reqBody struct {
+		ThreadTitle string `json:"threadTitle"`
+		CategoryId  string `json:"categoryId"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	newThreadId := models.PostThreadSql(accessToken, reqBody.ThreadTitle, reqBody.CategoryId)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(newThreadId)
 }
 
 // スレッド取得
-func getThreadById(id string) *models.ThreadAndComments {
+func getThreadById(w http.ResponseWriter, r *http.Request, id string) {
 	fmt.Println("スレッド取得処理")
-	return models.GetThreadByIdSql(id)
+	thread := models.GetThreadByIdSql(id)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(thread)
 }
 
 // スレッドコメント作成
-func postThreadComments(id, userId, sessionId, commentTitle string) *models.CommentAndUser {
+func postThreadComments(w http.ResponseWriter, r *http.Request, threadId string) {
 	fmt.Println("スレッドコメント作成処理")
-	return models.PostCommentsSql(id, userId, sessionId, commentTitle)
+	var reqBody struct {
+		UserId       string `json:"userId"`
+		SessionId    string `json:"sessionId"`
+		CommentTitle string `json:"commentTitle"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		fmt.Println(err)
+	}
+	comment := models.PostCommentsSql(threadId, reqBody.UserId, reqBody.SessionId, reqBody.CommentTitle)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(comment)
 }
 
 // スレッドコメント作成
-func getThreadsCategories() *models.ThreadsCategories {
+func getThreadsCategories(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("スレッドカテゴリー一覧取得処理")
-	return models.GetThreadsCategoriesSql()
+	categories := models.GetThreadsCategoriesSql()
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(categories)
 }
