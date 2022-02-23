24b1ea2a095cbd8d1914ef1362ae92c4d19938c3
ryo-ima <i.riino5y@gmail.com>
2022-02-23 21:06:41 +0900

    add; エラーハンドリングしてみた（必須項目チェックやIDが数字かチェックだけ）

--- app/controllers/threadsController.go
+++ app/controllers/threadsController.go
@@ -4,10 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"regexp"
 	"strings"
 
 	"github.com/GrowthOdyssey/TechBoard-BE/app/models"
 )
+
+type ErrMsg struct {
+	ErrorMessage string `json:"message"`
+}
 
 // スレッドハンドラ
 func threadsHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,18 +33,31 @@ func threadsHandler(w http.ResponseWriter, r *http.Request) {
 func threadsIdHandler(w http.ResponseWriter, r *http.Request) {
 	allowCors(w)
 	id := strings.TrimSuffix(strings.TrimPrefix(r.URL.Path, "/v1/threads/"), "/comments")
-	switch r.Method {
-	case http.MethodGet:
-		getThreadById(w, r, id)
-	// MEMO URLにcomments入っているか判定してハンドリングしたいかも
-	case http.MethodPost:
-		postThreadComments(w, r, id)
-	case http.MethodOptions:
-		w.WriteHeader(http.StatusOK)
+	switch {
+	case id == "":
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(ErrMsg{"パスにIDがありません"})
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	case regexp.MustCompile(`[^0-9]`).Match([]byte(id)):
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(ErrMsg{"パスは数字で指定してください"})
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 	default:
-		// TODO aiharanaoya
-		// 仮で500のStatusTextを返している。今後エラーハンドリングを実装。
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		switch r.Method {
+		case http.MethodGet:
+			getThreadById(w, r, id)
+		// MEMO URLにcomments入っているか判定してハンドリングしたいかも
+		case http.MethodPost:
+			postThreadComments(w, r, id)
+		case http.MethodOptions:
+			w.WriteHeader(http.StatusOK)
+		default:
+			// TODO aiharanaoya
+			// 仮で500のStatusTextを返している。今後エラーハンドリングを実装。
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
 	}
 }
 
@@ -63,9 +81,40 @@ func threadsCategoriesHandler(w http.ResponseWriter, r *http.Request) {
 // スレッド一覧取得
 func getThreads(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("スレッド一覧取得処理")
+	var errMsgAndErrors struct {
+		ErrMessage string `json:"message"`
+		Errors     struct {
+			CategoryId string `json:"categoryId"`
+			Page       string `json:"page"`
+			PerPage    string `json:"perPage"`
+		} `json:"errors"`
+	}
 	categoryId := r.FormValue("categoryId")
+	if regexp.MustCompile(`[^0-9]`).Match([]byte(categoryId)) {
+		errMsgAndErrors.Errors.CategoryId = "categoryIdは数字で指定してください"
+	}
 	page := r.FormValue("page")
+	if page == "" {
+		errMsgAndErrors.Errors.Page = "pageを指定してください"
+	} else if regexp.MustCompile(`[^0-9]`).Match([]byte(page)) {
+		errMsgAndErrors.Errors.Page = "pageは数字で指定してください"
+	}
 	perPage := r.FormValue("perPage")
+	if perPage == "" {
+		errMsgAndErrors.Errors.PerPage = "perPageを指定してください"
+	} else if regexp.MustCompile(`[^0-9]`).Match([]byte(perPage)) {
+		errMsgAndErrors.Errors.PerPage = "perPageは数字で指定してください"
+	}
+
+	if errMsgAndErrors.Errors.CategoryId+errMsgAndErrors.Errors.Page+errMsgAndErrors.Errors.PerPage != "" {
+		errMsgAndErrors.ErrMessage = "値が不正です"
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(422)
+		json.NewEncoder(w).Encode(errMsgAndErrors)
+		http.Error(w, http.StatusText(http.StatusUnprocessableEntity), http.StatusUnprocessableEntity)
+		return
+	}
+
 	threads := models.GetThreadsSql(categoryId, page, perPage)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
@@ -75,9 +124,17 @@ func getThreads(w http.ResponseWriter, r *http.Request) {
 // スレッド作成
 func postThread(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("スレッド作成処理")
+	var errMsgAndErrors struct {
+		ErrMessage string `json:"message"`
+		Errors     struct {
+			AccessToken string `json:"accessToken"`
+			ThreadTitle string `json:"threadTitle"`
+			CategoryId  string `json:"categoryId"`
+		} `json:"errors"`
+	}
 	accessToken := r.Header.Get("accessToken")
 	if accessToken == "" {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		errMsgAndErrors.Errors.AccessToken = "accessTokenがありません"
 	}
 
 	var reqBody struct {
@@ -87,6 +144,22 @@ func postThread(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&reqBody)
 	if err != nil {
 		fmt.Println(err)
+	}
+	if reqBody.ThreadTitle == "" {
+		errMsgAndErrors.Errors.ThreadTitle = "threadTitleを入力してください"
+	}
+	if reqBody.CategoryId == "" {
+		errMsgAndErrors.Errors.CategoryId = "categoryIdを選択してください"
+	} else if regexp.MustCompile(`[^0-9]`).Match([]byte(reqBody.CategoryId)) {
+		errMsgAndErrors.Errors.CategoryId = "categoryIdは数字で指定してください"
+	}
+	if errMsgAndErrors.Errors.AccessToken+errMsgAndErrors.Errors.ThreadTitle+errMsgAndErrors.Errors.CategoryId != "" {
+		errMsgAndErrors.ErrMessage = "値が不正です"
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(422)
+		json.NewEncoder(w).Encode(errMsgAndErrors)
+		http.Error(w, http.StatusText(http.StatusUnprocessableEntity), http.StatusUnprocessableEntity)
+		return
 	}
 
 	newThreadId := models.PostThreadSql(accessToken, reqBody.ThreadTitle, reqBody.CategoryId)
@@ -107,6 +180,14 @@ func getThreadById(w http.ResponseWriter, r *http.Request, id string) {
 // スレッドコメント作成
 func postThreadComments(w http.ResponseWriter, r *http.Request, threadId string) {
 	fmt.Println("スレッドコメント作成処理")
+	var errMsgAndErrors struct {
+		ErrMessage string `json:"message"`
+		Errors     struct {
+			UserId       string `json:"userId"`
+			SessionId    string `json:"sessionId"`
+			CommentTitle string `json:"commentTitle"`
+		} `json:"errors"`
+	}
 	var reqBody struct {
 		UserId       string `json:"userId"`
 		SessionId    string `json:"sessionId"`
@@ -116,15 +197,31 @@ func postThreadComments(w http.ResponseWriter, r *http.Request, threadId string)
 	if err != nil {
 		fmt.Println(err)
 	}
+	if reqBody.UserId == "" && reqBody.SessionId == "" {
+		errMsgAndErrors.Errors.UserId = "userIdを入力してください"
+		errMsgAndErrors.Errors.SessionId = "sessionIdを入力してください"
+	}
+	if reqBody.CommentTitle == "" {
+		errMsgAndErrors.Errors.CommentTitle = "commentTitleを入力してください"
+	}
+	if errMsgAndErrors.Errors.UserId+errMsgAndErrors.Errors.SessionId+errMsgAndErrors.Errors.CommentTitle != "" {
+		errMsgAndErrors.ErrMessage = "値が不正です"
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(422)
+		json.NewEncoder(w).Encode(errMsgAndErrors)
+		http.Error(w, http.StatusText(http.StatusUnprocessableEntity), http.StatusUnprocessableEntity)
+		return
+	}
 	comment := models.PostCommentsSql(threadId, reqBody.UserId, reqBody.SessionId, reqBody.CommentTitle)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(comment)
 }
 
-// スレッドコメント作成
+// スレッドカテゴリー一覧取得
 func getThreadsCategories(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("スレッドカテゴリー一覧取得処理")
+	//id := r.FormValue("id")
 	categories := models.GetThreadsCategoriesSql()
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
